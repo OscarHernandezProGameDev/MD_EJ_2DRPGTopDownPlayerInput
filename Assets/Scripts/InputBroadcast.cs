7b32f842bf5ad19a85161d137966108b458845d1
Oscar Devs <ohernandezba@gmail.com>
2024-05-12 13:41:56 +0200

    Broadcast Messages
    Co-Authored-By: Oscar Hernández <167362830+OscarHernandezProGameDev@users.noreply.github.com>

---
+++ Assets/Scripts/InputBroadcast.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputBroadcast : MonoBehaviour
+{
+    void OnExample()
+    {
+        Debug.Log("Broadcast funciona correctamente");
+    }
+}
